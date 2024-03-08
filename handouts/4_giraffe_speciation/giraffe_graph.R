6cfec1e3b37e02b152907cee4d2d18b95f203993
Michael S. Taylor <mtaylor@semo.edu>
2024-03-08 12:53:34 -0600

    Better plots for parts 1 and 2. Fix font name

--- handouts/4_giraffe_speciation/giraffe_graph.R
+++ handouts/4_giraffe_speciation/giraffe_graph.R
@@ -21,7 +21,7 @@ label_size = 13 # For x- and y-axis labels
 
 part1 <- c(1, 0, 6, 7, 1, 1, 3, 0, 0, 2) # after part 1
 #part <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10+) # to help me add values to the parts
-part2 <- c(0, 1, 1, 4, 8, 4, 0, 0, 0, 1) # after part 2
+part2 <- c(0, 1, 1, 5, 8, 4, 0, 0, 0, 1) # after part 2
 max_val <- max(part1, part2)
 
 giraffe_spp <- c("1","2","3","4","5", "6", "7", "8", "9", "10+")
@@ -35,7 +35,7 @@ df <- tibble(giraffe_spp, part1, part2) %>%
   
 
 # Load the extra fonts so they will be recognized for use.
-#loadfonts(device = "pdf")
+loadfonts(device = "pdf")
 
 # Part 1 plot (see below for Part 2) --------------------------------------
 giraffe_plot <- df %>% 
@@ -43,7 +43,7 @@ giraffe_plot <- df %>%
   ggplot(aes(x = giraffe_spp)) + 
   geom_col(aes(y = num_students, fill = part), 
            #fill = "#2171B5", # Fill color is Blue6 from Beamer lectures
-           width=0.7,
+           width = 0.7,
            position = "dodge") + 
   theme_tufte() + 
   scale_x_discrete(limits = sort(giraffe_spp, 
@@ -51,10 +51,10 @@ giraffe_plot <- df %>%
   labs(x = "Number of giraffe species",
        y = "Number of students",
        fill = "Part") +
-  scale_y_continuous(breaks=seq(1, max_val, 1)) + # Adjust largest
+  scale_y_continuous(breaks = seq(1, max_val, 1)) + # Adjust largest
   theme(axis.ticks = element_blank(), 
         axis.ticks.length = unit(0, "mm"),
-        text=element_text(size=label_size,  family="Linux Biolinum"),
+        text = element_text(size = label_size,  family = "Linux Biolinum O"),
         axis.text.x = element_text(margin = margin(b = 10))) +
   coord_flip() +
   scale_fill_manual(values = part1_colors,
@@ -70,11 +70,11 @@ giraffe_plot <- df %>%
 #y_intercept <- ggplot_build(giraffe_plot)$layout$coord$labels(ggplot_build(giraffe_plot)$layout$panel_params)[[1]]$x.major_source
 
 y_intercept <- ggplot_build(giraffe_plot)$layout$panel_params[[1]]$x.sec$breaks
-giraffe_plot <- giraffe_plot + geom_hline(yintercept = y_intercept, color="white", linewidth =  0.25)
+giraffe_plot <- giraffe_plot + geom_hline(yintercept = y_intercept, color = "white", linewidth =  0.25)
 
 giraffe_plot
 # Save the PDf file.
-ggsave(part1_plot, plot=giraffe_plot, width=4.5, height=3)
+ggsave(part1_plot, plot = giraffe_plot, width = 4.5, height = 3)
 
 
 # Part 1 plot (see below for Part 2) --------------------------------------
@@ -83,7 +83,7 @@ giraffe_plot <- df %>%
   ggplot(aes(x = giraffe_spp)) + 
   geom_col(aes(y = num_students, fill = part), 
            #fill = "#2171B5", # Fill color is Blue6 from Beamer lectures
-           width=0.7,
+           width = 0.7,
            position = "dodge") + 
   theme_tufte() + 
   scale_x_discrete(limits = sort(giraffe_spp, 
@@ -91,10 +91,10 @@ giraffe_plot <- df %>%
   labs(x = "Number of giraffe species",
        y = "Number of students",
        fill = "Part") +
-  scale_y_continuous(breaks=seq(1, max_val, 1)) + # Adjust largest
+  scale_y_continuous(breaks = seq(1, max_val, 1)) + # Adjust largest
   theme(axis.ticks = element_blank(), 
         axis.ticks.length = unit(0, "mm"),
-        text=element_text(size=label_size,  family="Linux Biolinum"),
+        text = element_text(size = label_size,  family = "Linux Biolinum O"),
         axis.text.x = element_text(margin = margin(b = 10))) +
   coord_flip() +
   scale_fill_manual(values = part2_colors,
@@ -102,12 +102,12 @@ giraffe_plot <- df %>%
                     labels = c("1", "2"))
 
 y_intercept <- ggplot_build(giraffe_plot)$layout$panel_params[[1]]$x.sec$breaks
-giraffe_plot <- giraffe_plot + geom_hline(yintercept = y_intercept, color="white", linewidth =  0.25)
+giraffe_plot <- giraffe_plot + geom_hline(yintercept = y_intercept, color = "white", linewidth =  0.25)
 
 giraffe_plot
 
 # Save the PDf file.
-ggsave(part2_plot, plot=giraffe_plot, width=4.5, height=3)
+ggsave(part2_plot, plot = giraffe_plot, width = 4.5, height = 3)
 
 
 # Embed the Linux Biolinum font.
